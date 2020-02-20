3bc3cd3a9fcbae2c56e06923ea09ef5c49400e20
Karol Bayas Pancho <kjbayas@f-l3210-pc03.aulas.etsit.urjc.es>
2020-02-20 09:55:27 +0100

    Practicando

---
+++ P2/L4/ejemplo1.js
@@ -0,0 +1,13 @@
+console.log('Ejecutando JS.....');
+const test=document.getElementById('test');
+
+test.onclick = () => {
+console.log("Click!!!");
+
+if (test.style.backgroundColor==""){
+  test.style.backgroundColor ="lightblue";
+}
+  else {
+    test.style.backgroundColor ="";
+  }
+}
---
+++ P2/L4/hello.js
@@ -0,0 +1,3 @@
+//es un comentario
+/*Estos son comentarios en bloque*/
+console.log("hola mundo");
