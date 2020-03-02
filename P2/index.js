ed6aa1020fd991299fb573e866392f9f5f1e2b83
Karol Bayas <kjbayas>
2020-03-03 00:53:45 +0100

    falta lo de ++++++ nan

--- P2/index.js
+++ P2/index.js
@@ -2,25 +2,32 @@ console.log("Ejecutando JS...");
 
 display = document.getElementById("display")
 igual = document.getElementById("igual")
-clear = document.getElementById("clear")
-del = document.getElementById("del")
+clear = document.getElementById("C")
+del = document.getElementById("CE")
 
 let boton = document.getElementsByClassName("tecla")
+
 for (i=0; i<boton.length; i++) {
   boton[i].onclick = (bot) => {
     display.innerHTML += bot.target.value;
   }
 }
-
+let simbolo = document.getElementsByClassName("sim")
+for (i=0; i<simbolo.length; i++) {
+  simbolo[i].onclick = (simb) => {
+    display.innerHTML += simb.target.value;
+  }
+}
 //-- Evaluar la expresion
 igual.onclick = () => {
   display.innerHTML = eval(display.innerHTML);
+
 }
 
 //-- Poner a cero la expresion
 clear.onclick = () => {
-  display.innerHTML = "0";
+  display.innerHTML = "";
 }
 del.onclick = () => {
-  display.innerHTML = "";
+  display.innerHTML = display.innerHTML.slice(0,-1);
 }
