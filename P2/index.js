0bc1af07c5d0da8231ecbb2a32e0cc6d430f4d1c
Karol Bayas Pancho <kjbayas@f-l3210-pc03.aulas.etsit.urjc.es>
2020-03-05 10:29:38 +0100

    sin maquina de estados

--- P2/index.js
+++ P2/index.js
@@ -1,33 +1,51 @@
 console.log("Ejecutando JS...");
 
+
 display = document.getElementById("display")
 igual = document.getElementById("igual")
 clear = document.getElementById("C")
 del = document.getElementById("CE")
 
-let boton = document.getElementsByClassName("tecla")
+const ESTADO = {
+  INIT: 0,
+  OP1: 1,
+  OPERATION: 2,
+  OP2_INIT: 3,
+  OP2: 4,
+}
 
+let boton = document.getElementsByClassName("tecla")
 for (i=0; i<boton.length; i++) {
   boton[i].onclick = (bot) => {
     display.innerHTML += bot.target.value;
   }
 }
+
 let simbolo = document.getElementsByClassName("sim")
 for (i=0; i<simbolo.length; i++) {
   simbolo[i].onclick = (simb) => {
     display.innerHTML += simb.target.value;
   }
 }
-//-- Evaluar la expresion
+//Igual
 igual.onclick = () => {
   display.innerHTML = eval(display.innerHTML);
-
 }
-
-//-- Poner a cero la expresion
+//Eliminar Todo
 clear.onclick = () => {
   display.innerHTML = "";
 }
+// elimina Uno
 del.onclick = () => {
   display.innerHTML = display.innerHTML.slice(0,-1);
+}
+
+function number(num)
+{
+  //-- Segun el estado hacemos una cosa u otra
+  if (estado == ESTADO.INIT) {
+    display.innerHTML = num;
+    estado = ESTADO.OP1;
+  }
+  // .......... Resto del código
 }
