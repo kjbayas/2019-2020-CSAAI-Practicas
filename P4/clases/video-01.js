679e48d1dafbc4841a6547a5894094b3fa130569
Karol Bayas <kjbayas>
2020-04-20 19:22:03 +0200

    videos colocados

--- P4/clases/video-01.js
+++ P4/clases/video-01.js
@@ -20,8 +20,8 @@ video3.height=200;
 video3.src="https://github.com/kjbayas/videos/raw/master/video3.mp4";
 //video 4
 const video4 = document.getElementById("video4")
-video4.width=500;  //-- Tamaño de la pantalla de video
-video4.height=600;
+video4.width=700;  //-- Tamaño de la pantalla de video
+video4.height=300;
 video4.poster="https://github.com/kjbayas/videos/raw/master/dyp.png";
 //-- Obtener los botones
 const play1 = document.getElementById("play1")
@@ -31,6 +31,9 @@ play1.onclick = () => {
   video4.src=video1.src;
   video4.muted=false;
   video4.loop=false;
+  document.getElementById("video1").style.border = "thick solid blue";
+  document.getElementById("video2").style.border = "none";
+  document.getElementById("video3").style.border = "none";
 };
 const play2 = document.getElementById("play2")
 play2.onclick = () => {
@@ -38,6 +41,9 @@ play2.onclick = () => {
   video4.src=video2.src;
   video4.muted=false;
   video4.loop=false;
+  document.getElementById("video2").style.border = "thick solid blue";
+  document.getElementById("video3").style.border = "none";
+  document.getElementById("video1").style.border = "none";
 };
 const play3 = document.getElementById("play3")
 play3.onclick = () => {
@@ -45,4 +51,7 @@ play3.onclick = () => {
   video4.src=video3.src;
   video4.muted=false;
   video4.loop=false;
+  document.getElementById("video3").style.border = "thick solid blue";
+  document.getElementById("video1").style.border = "none";
+  document.getElementById("video2").style.border = "none";
 };
